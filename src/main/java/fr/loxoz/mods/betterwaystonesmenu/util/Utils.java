b68e5d55551f8eca5b04942459f64c84bf2fd5cf
Cremo <dcremon36@gmail.com>
2026-03-08 14:53:11 -0300

    Update Utils.java

--- src/main/java/fr/loxoz/mods/betterwaystonesmenu/util/Utils.java
+++ src/main/java/fr/loxoz/mods/betterwaystonesmenu/util/Utils.java
@@ -1,9 +1,8 @@
 package fr.loxoz.mods.betterwaystonesmenu.util;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import fr.loxoz.mods.betterwaystonesmenu.compat.CText;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.MutableComponent;
 
 public class Utils {
@@ -12,18 +11,19 @@ public class Utils {
         boolean trimmed = false;
         while (font.width(out) > maxWidth) {
             trimmed = true;
-            out = out.substring(0, out.length()-1);
+            out = out.substring(0, out.length() - 1);
         }
         if (trimmed) {
-            out = out.substring(0, out.length()-3) + "...";
+            out = out.substring(0, out.length() - 3) + "...";
         }
         return CText.literal(out);
     }
 
-    public static void drawOutline(PoseStack matrices, int x1, int y1, int x2, int y2, int color) {
-        GuiComponent.fill(matrices, x1, y1, x2 - 1, y1 + 1, color);
-        GuiComponent.fill(matrices, x2 - 1, y1, x2, y2 - 1, color);
-        GuiComponent.fill(matrices, x2, y2 - 1, x1 + 1, y2, color);
-        GuiComponent.fill(matrices, x1, y2, x1 + 1, y1 + 1, color);
+    // PoseStack → GuiGraphics, GuiComponent.fill → guiGraphics.fill
+    public static void drawOutline(GuiGraphics guiGraphics, int x1, int y1, int x2, int y2, int color) {
+        guiGraphics.fill(x1,     y1,     x2 - 1, y1 + 1, color); // top
+        guiGraphics.fill(x2 - 1, y1,     x2,     y2 - 1, color); // right
+        guiGraphics.fill(x2,     y2 - 1, x1 + 1, y2,     color); // bottom
+        guiGraphics.fill(x1,     y2,     x1 + 1, y1 + 1, color); // left
     }
 }
