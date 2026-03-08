b4e398105cef2f652fac175e51e15b4e7f735e75
Cremo <dcremon36@gmail.com>
2026-03-08 01:55:06 -0300

    Update BetterWaystoneSelectionScreen.java

--- src/main/java/fr/loxoz/mods/betterwaystonesmenu/gui/screen/BetterWaystoneSelectionScreen.java
+++ src/main/java/fr/loxoz/mods/betterwaystonesmenu/gui/screen/BetterWaystoneSelectionScreen.java
@@ -1,8 +1,10 @@
 package fr.loxoz.mods.betterwaystonesmenu.gui.screen;
 
 import net.blay09.mods.waystones.menu.WaystoneSelectionMenu;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * The upgraded version of {@link net.blay09.mods.waystones.client.gui.screen.WaystoneSelectionScreen}
@@ -11,4 +13,8 @@ public class BetterWaystoneSelectionScreen extends BetterWaystoneSelectionScreen
     public BetterWaystoneSelectionScreen(WaystoneSelectionMenu container, Inventory playerInventory, Component title) {
         super(container, playerInventory, title);
     }
+
+    // AbstractContainerScreen exige essa implementação no 1.21
+    @Override
+    protected void renderBg(@NotNull GuiGraphics guiGraphics, float delta, int mouseX, int mouseY) {}
 }
