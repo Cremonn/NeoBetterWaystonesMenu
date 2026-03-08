9530d4a0e7d2aed909ace2972247b79cc9650bc0
Cremo <dcremon36@gmail.com>
2026-03-08 14:49:27 -0300

    Update CText.java

--- src/main/java/fr/loxoz/mods/betterwaystonesmenu/compat/CText.java
+++ src/main/java/fr/loxoz/mods/betterwaystonesmenu/compat/CText.java
@@ -1,28 +1,19 @@
 package fr.loxoz.mods.betterwaystonesmenu.compat;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.Component.literal;
-import net.minecraft.network.chat.Component.translatable;
 
-/**
- * I usually make this utility file for large projects that could support multiple versions of Minecraft to make
- * maintenance easier across multiple versions of Minecraft and also because I like the Minecraft 1.19+ unique Text
- * class for all text components.
- */
 public class CText {
     public static MutableComponent literal(String string) {
-        return new Component.literal(string);
+        return Component.literal(string);
     }
-
     public static MutableComponent translatable(String key) {
-        return new Component.translatable(key);
+        return Component.translatable(key);
     }
-
     public static MutableComponent translatable(String key, Object ...args) {
-        return new Component.translatable(key, args);
+        return Component.translatable(key, args);
     }
-
     public static MutableComponent empty() {
-        return new Component.literal("");
+        return Component.literal("");
     }
 }
