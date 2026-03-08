a78dc2c0095e875f016bc27a37218b428cc2f552
Cremo <dcremon36@gmail.com>
2026-03-08 14:53:53 -0300

    Update WaystoneUtils.java

--- src/main/java/fr/loxoz/mods/betterwaystonesmenu/util/WaystoneUtils.java
+++ src/main/java/fr/loxoz/mods/betterwaystonesmenu/util/WaystoneUtils.java
@@ -1,14 +1,14 @@
 package fr.loxoz.mods.betterwaystonesmenu.util;
 
 import fr.loxoz.mods.betterwaystonesmenu.compat.CText;
-import net.blay09.mods.waystones.api.IWaystone;
+import net.blay09.mods.waystones.api.Waystone; // IWaystone → Waystone
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.MutableComponent;
 
 public class WaystoneUtils {
-    public static MutableComponent getTrimmedWaystoneName(IWaystone waystone, Font font, int maxWidth) {
+    public static MutableComponent getTrimmedWaystoneName(Waystone waystone, Font font, int maxWidth) {
         if (!waystone.hasName()) return CText.translatable("gui.waystones.waystone_selection.unnamed_waystone").withStyle(s -> s.withColor(ChatFormatting.GRAY));
-        return Utils.trimTextWidth(waystone.getName(), font, maxWidth);
+        return Utils.trimTextWidth(waystone.getName().getString(), font, maxWidth); // getName() → getName().getString()
     }
 }
