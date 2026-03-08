24ea260c60fff3c7cb4fd605bc98f10b519184ba
Cremo <dcremon36@gmail.com>
2026-03-08 15:56:36 -0300

    Update TexturedButtonTooltipWidget.java

--- src/main/java/fr/loxoz/mods/betterwaystonesmenu/compat/widget/TexturedButtonTooltipWidget.java
+++ src/main/java/fr/loxoz/mods/betterwaystonesmenu/compat/widget/TexturedButtonTooltipWidget.java
@@ -1,25 +1,37 @@
 package fr.loxoz.mods.betterwaystonesmenu.compat.widget;
 
 import fr.loxoz.mods.betterwaystonesmenu.compat.tooltip.IPositionedTooltipProvider;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class TexturedButtonTooltipWidget extends ImageButton implements IPositionedTooltipProvider {
-    public TexturedButtonTooltipWidget(int x, int y, int width, int height, int u, int v, int hoverVOffset, ResourceLocation texture, int textureWidth, int textureHeight, Button.OnPress onClick, Component message) {
-        super(x, y, width, height, u, v, hoverVOffset, texture, textureWidth, textureHeight, onClick, message);
+public class TexturedButtonTooltipWidget extends Button implements IPositionedTooltipProvider {
+    private final ResourceLocation texture;
+    private final int u, v, hoverVOffset, textureWidth, textureHeight;
+
+    public TexturedButtonTooltipWidget(int x, int y, int width, int height, int u, int v, int hoverVOffset, ResourceLocation texture, int textureWidth, int textureHeight, OnPress onClick, Component message) {
+        super(x, y, width, height, message, onClick, Button.DEFAULT_NARRATION);
+        this.u = u;
+        this.v = v;
+        this.hoverVOffset = hoverVOffset;
+        this.texture = texture;
+        this.textureWidth = textureWidth;
+        this.textureHeight = textureHeight;
     }
 
     @Override
-    public boolean shouldShowTooltip() {
-        return isHoveredOrFocused();
+    public void renderWidget(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        int vOffset = isHoveredOrFocused() ? v + hoverVOffset : v;
+        guiGraphics.blit(texture, getX(), getY(), u, vOffset, width, height, textureWidth, textureHeight);
     }
 
     @Override
-    public List<Component> getTooltip() {
-        return List.of(getMessage());
-    }
+    public boolean shouldShowTooltip() { return isHoveredOrFocused(); }
+
+    @Override
+    public List<Component> getTooltipComponents() { return List.of(getMessage()); }
 }
