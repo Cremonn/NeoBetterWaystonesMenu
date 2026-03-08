1c0cc48d6be8d4f32b8f71cc2f503a3162163c4e
Cremo <dcremon36@gmail.com>
2026-03-08 15:34:40 -0300

    Update IPositionedTooltipProvider.java

--- src/main/java/fr/loxoz/mods/betterwaystonesmenu/compat/tooltip/IPositionedTooltipProvider.java
+++ src/main/java/fr/loxoz/mods/betterwaystonesmenu/compat/tooltip/IPositionedTooltipProvider.java
@@ -3,22 +3,20 @@ package fr.loxoz.mods.betterwaystonesmenu.compat.tooltip;
 import net.blay09.mods.waystones.client.gui.widget.ITooltipProvider;
 import net.minecraft.client.gui.components.AbstractWidget;
 
-/**
- * using {@link ITooltipProvider} as base type for compatibility with the waystones mod
- */
-public interface IPositionedTooltipProvider extends ITooltipProvider {
-    default TooltipPos getTooltipPos(int mouseX, int mouseY) {
-        return getTooltipPos(mouseX, mouseY, new TooltipOffset(0, 0));
-    }
+import java.util.List;
 
-    default TooltipPos getTooltipPos(int mouseX, int mouseY, TooltipOffset widgetOffset) {
-        if (this instanceof AbstractWidget widget && widget.isFocused()) {
-            return getFromWidget(widget, widgetOffset);
+public interface IPositionedTooltipProvider extends ITooltipProvider {
+
+    @Override
+    boolean shouldShowTooltip();
+
+    @Override
+    List<net.minecraft.network.chat.Component> getTooltipComponents();
+
+    default TooltipPos getTooltipPos(int mouseX, int mouseY, TooltipOffset offset) {
+        if (this instanceof AbstractWidget widget) {
+            return new TooltipPos(widget.getX() + offset.x(), widget.getY() + offset.y());
         }
         return new TooltipPos(mouseX, mouseY);
-    }
-
-    static TooltipPos getFromWidget(AbstractWidget widget, TooltipOffset offset) {
-        return new TooltipPos(widget.x + offset.x(), widget.y + offset.y());
     }
 }
